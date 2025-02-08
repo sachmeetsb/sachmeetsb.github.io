66caa42ec05fb691e2cfd13f5114af08311d9b46
Sachmeet <sachmeetsb@gmail.com>
2025-02-08 23:23:23 +0530

    Website7

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kartar.ai/"
+  base: "/sachmeetsb.github.io/"
 })
